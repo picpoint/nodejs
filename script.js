27f278fd2ccf28c7f3f379dcc61029891173b7e7
picpoint <picpoint@yandex.ru>
2019-07-26 16:53:16 +0300

    express & handlebars

--- script.js
+++ script.js
@@ -2,13 +2,26 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 
-app.use(express.static('dist'));
 app.use(bodyParser.urlencoded());
 app.use(bodyParser.json());
-
-
-app.get('/:lin', (req, res) => {
-	res.send('Hello Nodejs ' + req.params.lin);
+app.set('view engine', 'hbs');
+app.set('views', 'dist');
+app.use('/contact', (req, res) => {
+  res.render('contact.hbs', {
+    title: 'Мои контакты',
+    email: 'inbox@inbox.ru',
+    phone: ['+79051234567', '+79880886427', '+79280885577'],
+    adress: 'Piter city'
+  });
 });
+app.use('/', (req, res) => {
+  res.send('GENERAL PAGE!');
+});
+
+/*
+app.get('/index.html', (req, res) => {
+	res.send('Hello Nodejs ');
+});
+*/
 
 app.listen(4000);
